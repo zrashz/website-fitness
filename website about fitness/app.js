9af2b0340cb0eba9b7d48b4ce87d7c238524694b
Rashmi Kaveesha <152951952+zrashz@users.noreply.github.com>
2024-06-19 07:46:04 +0530

    Add files via upload

---
+++ website about fitness/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.querySelector('.nav-toggle');
+  const nav = document.querySelector('nav');
+
+  navToggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
+});
